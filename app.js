30667d9143982d1775b16285837fec988d359793
cexbrayat <cedric@ninja-squad.com>
2013-11-15 13:43:22 +0100

    socketio

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 var express = require('express')
   , app = express()
-  , server = require('http').createServer(app);
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
 
 app.use(express.static(__dirname + '/'));
 
 server.listen(9003);
+
+var votes = [ 
+  { choice: 1, votes: 0 },
+  { choice: 2, votes: 0 },
+  { choice: 3, votes: 0 },
+  { choice: 4, votes: 0 },
+  { choice: 5, votes: 1 }
+];
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('votes', { votes: votes });
+  socket.on('vote', function(msg){
+  	console.log(msg);
+  	votes[msg.vote-1].votes++;
+  	io.sockets.emit('votes', { votes: votes });
+  })
+});
